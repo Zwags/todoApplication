50ed42b12129b14fedd044cf0be95c36018c036e
zwags <zachary.wagner18@gmail.com>
2017-11-29 23:56:54 -0500

    Basic functionality finshed

---
+++ assets/js/todos.js
@@ -0,0 +1,23 @@
+//Check off todo's via click
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("completed");
+});
+
+//Clicking on X delete's todo's, watch for event bubbling
+$("ul").on("click", "span", function(event) {
+  $(this).parent().fadeOut(700, function() {
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13 ) {
+      //grab new text
+    var todoText =  $(this).val();
+    $(this).val("");
+    //create new li add to ul
+    $("ul").append("<li> <span> X </span> "+ todoText +"</li>")
+    }
+});
