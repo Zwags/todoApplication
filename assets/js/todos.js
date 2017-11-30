d00a36d5101c830f9493c748212ecc300249de5d
zwags <zachary.wagner18@gmail.com>
2017-11-30 00:36:57 -0500

    Finished, added last of jQuery needs

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -18,6 +18,10 @@ $("input[type='text']").keypress(function(event){
     var todoText =  $(this).val();
     $(this).val("");
     //create new li add to ul
-    $("ul").append("<li> <span> X </span> "+ todoText +"</li>")
+    $("ul").append("<li> <span> <i class='fa fa-trash'></i> </span> "+ todoText +"</li>")
     }
+});
+
+$(".fa-plus").click(function(){
+  $("input[type='text']").fadeToggle(400);
 });
